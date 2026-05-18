a2ec266e35409badc366dfb3c794c36a1bda5608
Adrian <1925728@alu.murciaeduca.es>
2026-05-18 20:36:06 +0200

    v1.2

--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -3,7 +3,11 @@ package vista;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Iniciando programa");
+
+        System.out.println("Programa finalizado");
+
     }
 
 }
