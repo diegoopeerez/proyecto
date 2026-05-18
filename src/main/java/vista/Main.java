b436bca28d82cdd50c3fb996067ee67678a9b234
Diego <diego@dam.com>
2026-05-18 20:38:58 +0200

    v2.0

--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Iniciando programa");
 
-        System.out.println("Programa finalizado");
-        System.out.println("Putero");
 
     }
 
